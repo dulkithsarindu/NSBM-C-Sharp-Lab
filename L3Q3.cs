18f271ebeb058d5be16f9a6147789780742cfbe0
dulkithsarindu <135964220+dulkithsarindu@users.noreply.github.com>
2024-02-27 20:03:25 +0530

    Add files via upload

---
+++ L3Q3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3
+{
+    class product
+    {
+        private int productid;
+        private string productName;
+        private int price;
+        private int productQuantity;
+
+        public product(int Productid, string ProductName, int Price, int ProductQuantity)
+        {
+            productid = Productid;
+            productName = ProductName;
+            price = Price;
+            productQuantity = ProductQuantity;
+        }
+        public int Productid
+        {
+            get
+            {
+                return productid;
+            }
+        }
+        public string ProductName
+        {
+            get
+            {
+                return productName;
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+        }
+        public int ProductQuantity
+        {
+            get
+            {
+                return productQuantity;
+            }
+        }
+        public void AddProducts()
+        {
+            productQuantity += 1;
+        }
+        public void BuyProducts()
+        {
+            productQuantity -= 1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            product laptop = new product(101, "Laptop", 800, 10);
+
+            laptop.AddProducts();
+
+            Console.WriteLine($"Product id: {laptop.Productid}");
+            Console.WriteLine($"Product Name: {laptop.ProductName}");
+            Console.WriteLine($"Price: {laptop.Price}");
+            Console.WriteLine($"Quantity: {laptop.ProductQuantity}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ L3Q4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4
+{
+    abstract class shape
+    {
+        public string shapeType;
+        public double area;
+
+        public abstract double CalculateArea();
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Shapetype: {shapeType}");
+            Console.WriteLine($"Area: {area}");
+        }
+    }
+    class rectangle : shape
+    {
+        public double length;
+        public double width;    
+        public void setDimensions(double length, double width)
+        {
+            this.length = length;
+            this.width = width; 
+        }
+        public override double CalculateArea()
+        {
+            area = length * width;
+            return area;
+        }
+    }
+    class circle : shape
+    {
+        public double radius;
+
+        public void setDimensions(double radius)
+        {
+            this.radius = radius;   
+        }
+        public override double CalculateArea()
+        {
+            area = radius * radius * 3.14;
+            return area;    
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rectangle = new rectangle();
+            rectangle.setDimensions(5, 10);
+            rectangle.CalculateArea();
+            Console.WriteLine($"Area of rectangle: {rectangle.CalculateArea()}");
+
+            circle circle = new circle();
+            circle.setDimensions(5);
+            circle.CalculateArea();
+            Console.WriteLine($"Area of circle: {circle.CalculateArea()}");
+
+            Console.ReadLine();
+        }
+    }
+}
